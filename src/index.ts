be4629e47da165a27ed039066e1f3682ada63ab8
kevily <bug_zero@163.com>
2021-07-18 23:12:22 +0800

    ts,babel,copy

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,24 @@ import { SrcOptions } from 'vinyl-fs'
 import path from 'path'
 import ts from 'gulp-typescript'
 import babel from 'gulp-babel'
+import terser from 'gulp-terser'
 
 export interface paramsType {
     root?: string
     output?: string
 }
 
-function genScriptSrc(root: paramsType['root']) {
-    return ['ts', 'js', 'tsx', 'jsx'].map(s => path.join(root, `src/**/*.${s}`))
-}
-
 class Task {
     public root: paramsType['root']
+    public srcRoot: string
+    public scriptSrc: string[]
     public output: paramsType['output']
     public ignore: string[]
     public srcDefaultOps: SrcOptions
     constructor(params: paramsType) {
         this.root = params?.root || process.cwd()
+        this.srcRoot = path.join(this.root, 'src')
+        this.scriptSrc = ['ts', 'js', 'tsx', 'jsx'].map(s => `**/*.${s}`)
         this.output = params?.output || path.join(this.root, 'dist')
         this.ignore = [
             path.join(this.root, `**/node_modules/**/*.*`),
@@ -28,32 +29,29 @@ class Task {
         ]
         this.srcDefaultOps = {
             ignore: this.ignore,
-            cwd: this.root
+            cwd: path.join(this.root, 'src')
         }
     }
-    ts(isExportJs = true) {
+    ts() {
         const tsProject = ts.createProject(path.join(this.root, 'tsconfig.json'))
-        const tsTask = gulp.src(genScriptSrc(this.root), this.srcDefaultOps).pipe(tsProject())
-        if (isExportJs) {
-            tsTask.js.pipe(gulp.dest(this.output))
-        }
+        const tsTask = gulp.src(this.scriptSrc, this.srcDefaultOps).pipe(tsProject())
+        tsTask.js.pipe(gulp.dest(this.output))
         return tsTask.dts.pipe(gulp.dest(this.output))
     }
     babel() {
-        return gulp
-            .src(genScriptSrc(this.root), this.srcDefaultOps)
-            .pipe(
-                babel({
-                    presets: ['@babel/preset-typescript', '@babel/preset-env'],
-                    plugins: ['@babel/plugin-transform-runtime']
-                })
-            )
-            .pipe(gulp.dest(this.output))
+        let task = gulp.src(this.scriptSrc, this.srcDefaultOps)
+        task = task.pipe(
+            babel({
+                presets: ['@babel/preset-typescript', '@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime']
+            })
+        )
+        return task.pipe(gulp.dest(this.output))
     }
     copy(ext: string[]) {
-        const files = ext.map(s => path.join(this.root, `src/**/*.${s}`))
+        const files = ext.map(s => `**/*.${s}`)
         return gulp.src(files, this.srcDefaultOps).pipe(gulp.dest(this.output))
     }
 }
 
-export { Task, genScriptSrc }
+export default Task
