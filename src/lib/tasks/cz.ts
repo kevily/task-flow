fc8342f4e1bd3ece97c5a7fb002e686be2526296
kevily <bug_zero@163.com>
2021-08-22 15:28:40 +0800

    Fix: cz bug

--- src/lib/tasks/cz.ts
+++ src/lib/tasks/cz.ts
@@ -1,10 +1,13 @@
 import { bootstrap } from 'commitizen/dist/cli/git-cz'
 
 export default async function () {
-    bootstrap({
-        cliPath: require.resolve('commitizen').replace(/\/commitizen\/.*/g, '/commitizen'),
-        config: {
-            path: require.resolve('cz-adapter-eslint')
-        }
-    })
+    bootstrap(
+        {
+            cliPath: require.resolve('commitizen').replace(/\/commitizen\/.*/g, '/commitizen'),
+            config: {
+                path: require.resolve('cz-adapter-eslint')
+            }
+        },
+        process.argv.slice(0, 2)
+    )
 }
