abbf2b215def50591a71cc25e9dca0925f96621e
1k <bug_zero@163.com>
2022-04-30 22:50:54 +0800

    v2.1.2

--- src/lib/tasks/cz.ts
+++ src/lib/tasks/cz.ts
@@ -1,13 +1,20 @@
-import { bootstrap } from 'commitizen/dist/cli/git-cz'
+import { EngineConfigType } from '../Engine'
+import path = require('path')
+import fsExtra = require('fs-extra')
+import { assign } from 'lodash'
+import { resolvePackage } from '../utils'
 
-export default async function () {
-    bootstrap(
-        {
-            cliPath: require.resolve('commitizen').replace(/\/commitizen\/.*/g, '/commitizen'),
-            config: {
-                path: require.resolve('cz-adapter-eslint')
-            }
-        },
-        process.argv.slice(0, 2)
-    )
+export interface czConfigType extends EngineConfigType {}
+export default async function cz(c: czConfigType) {
+    if (!resolvePackage('commitizen') || !resolvePackage('cz-adapter-eslint')) {
+        throw new Error('Please install commitizen and cz-adapter-eslint')
+    }
+    const pkgPath = path.join(c.root || process.cwd(), 'package.json')
+    const pkg: { [key: string]: any } = fsExtra.readJSONSync(pkgPath)
+    pkg.config = assign({}, pkg.config, {
+        commitizen: {
+            path: './node_modules/cz-adapter-eslint'
+        }
+    })
+    fsExtra.writeJSONSync(pkgPath, pkg, { spaces: 4 })
 }
